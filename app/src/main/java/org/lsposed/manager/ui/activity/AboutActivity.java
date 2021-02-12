7804a1e7dd52e3c1393e6c9fd02efbf2bcba4b13
naicfeng <blog@weicn.org>
2021-02-13 01:44:03 +0800

    up AboutActivity

--- app/src/main/java/org/lsposed/manager/ui/activity/AboutActivity.java
+++ app/src/main/java/org/lsposed/manager/ui/activity/AboutActivity.java
@@ -5,12 +5,8 @@ import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 
-import com.bumptech.glide.Glide;
-
-import org.lsposed.manager.BuildConfig;
 import org.lsposed.manager.R;
 import org.lsposed.manager.databinding.ActivityAboutBinding;
-import org.lsposed.manager.util.GlideHelper;
 import org.lsposed.manager.util.NavUtil;
 
 public class AboutActivity extends BaseActivity {
@@ -21,22 +17,16 @@ public class AboutActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityAboutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-//        setSupportActionBar(binding.toolbar);
-//        binding.toolbar.setNavigationOnClickListener(view -> finish());
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationOnClickListener(view -> finish());
         ActionBar bar = getSupportActionBar();
         if (bar != null) {
             bar.setDisplayHomeAsUpEnabled(true);
         }
 
-        binding.appVersion.setText(BuildConfig.VERSION_NAME);
-
         setupView(binding.sourceCodeView, R.string.about_source);
         setupView(binding.tgChannelView, R.string.group_telegram_channel_link);
-		setupView(binding.modifySupportView, R.string.about_cuojue_link);
-
-        Glide.with(binding.appIcon)
-                .load(GlideHelper.wrapApplicationInfoForIconLoader(getApplicationInfo()))
-                .into(binding.appIcon);
+        setupView(binding.modifySupportView, R.string.about_cuojue_link);
     }
 
     void setupView(View v, final int url) {
